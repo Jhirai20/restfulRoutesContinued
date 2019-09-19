e5f4de83a4327881b3f1c1b61d89ee0ef8ad2b4e
Jeffrey Hirai <jhirai20@gmail.com>
2019-09-19 10:06:13 -0500

    Restful Tasks API -nested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService} from './http.service'
+import { HttpService } from './http.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  //must declare and type all new varibles
-  title = 'MEAN';
-  tasks = []
-  task="" //needed to declare string varible 
-  newTask: any;
-  editTask: any;
-  editTog: boolean = false;
-  green = false
-  constructor(private _httpService: HttpService){}
 
-  ngOnInit(){
+export class AppComponent implements OnInit {
+    constructor(private _httpService: HttpService){}
+    title = "Restful Tasks API"
+    tasks = [];
+    task = "";
+    selectedTask;
+
+    ngOnInit(){
     
-    //this function gets called after init constructor
-    this.getTasksFromService()
-    this.newTask = { title: "", description: "", completed: false }
-  }
-  onSubmit(){
-    let observable = this._httpService.addTask(this.newTask)
-    observable.subscribe(data =>{
-      console.log("-Create-",data)
-      this.newTask = { title: "", description: "" }
-    })
-    this.getTasksFromService()
-  }
-  getTasksFromService(){
-    let observable = this._httpService.getTasks()
-    observable.subscribe(data =>{
-      console.log("got our data!",data)
-      this.tasks = data['task']
-    })
-  }
-  info(idx){
-    this.task = this.tasks[idx]
-    this.green = true
-  }
-  delete(idx){
-    let observable = this._httpService.getTasks()
-    observable.subscribe(data =>{
-      console.log("got our data!",data)
-      this.tasks = data['task']
-    })
-  }
-    onDelete(task){
-      let observable = this._httpService.deleteTask(task);
-      observable.subscribe(data => {
-        console.log("~Delete~");
-        this.getTasksFromService();
-    })
-  }
+    }
 
-  editForm(task){
-    this.editTask = {_id: task._id, title: task.title, description: task.description}
-    this.editTog = true;
-  }
+    getTasksFromService(){
+      let observable = this._httpService.getTasks();
+      observable.subscribe(data => 
+        {console.log("Got our tasks!", data)
+        this.tasks = data["task"]
+        console.log(data)
+        console.log(data["task"])
+      });
+    }
 
-  onEdit(){
-    let observable = this._httpService.editTask(this.editTask);
-    observable.subscribe(data => {
-      console.log("~Edit~");
-      this.editTog = false;
-      this.getTasksFromService();
-    })
-  }
-  // do($event){
-  //   console.log($event)
-  // }
-  // onButtonClick(): void { 
-  //   console.log(`Click event is working`);
-  // }
-  // onButtonClickParam(num: Number): void { 
-  //     console.log(`Click event is working with num param: ${num}`);
-  //     let observable = this._httpService.postToServer({data:num})
-  //     observable.subscribe(data => console.log("Got out data!", data))
-  // }
-  // onButtonClickParams(num: Number, str: String): void { 
-  //     console.log(`Click event is working with num param: ${num} and str param: ${str}`);
-  // }
-  // onButtonClickEvent(event: any): void { 
-  //     console.log(`Click event is working with event: ${event}`);
-  // }
-};
+    info(idx){
+      this.selectedTask = this.tasks[idx];
+    }
+}
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from  '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,29 +8,14 @@ import {HttpClient} from  '@angular/common/http'
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    // this.getTasks()
-   }
-   getTasks(){
-    //  let tempObservable = this._http.get('/tasks')
-    //  tempObservable.subscribe(data => console.log("Got our tasks!", data))
-    return  this._http.get('/tasks')
-   }
-  //  postToServer(num){
-  //    return this._http.post('/tasks',num)
-  //  }
-  getTaskById(id:string){
-    return this._http.get("/tasks/:id")
   }
-  addTask(newtask){
-    return this._http.post("/tasks", newtask)
+
+  getTasks(){
+    return this._http.get("/tasks");
+ }
+
+  getTasksById(id:string){
+    return this._http.get("/tasks/:id");
   }
-  removeTask(id:string){
-    return this._http.delete("/tasks/:id" )
-  }
-  editTask(editTask){
-    return this._http.put(`/tasks/${editTask._id}`, editTask);
-  }
-  deleteTask(task){
-    return this._http.delete(`/tasks/${task._id}`, task);
-  }
-}; 
+
+}
